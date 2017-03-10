9ec2c838fb75455c4c9dca28930cf3646159de62
Fara Putri Zhafira <fpzhafira@gmail.com>
2017-03-10 13:59:26 +0700

    initial commit

---
+++ src/test/java/com/prestasi/ModulPrestasiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prestasi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModulPrestasiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
